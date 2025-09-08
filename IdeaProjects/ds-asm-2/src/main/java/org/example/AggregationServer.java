91228a696d718ad47d9067000038308f8dc04f90
BaoAu <doraemonagb@gmail.com>
2025-09-08 21:06:25 +0930

    Implemented HTTP request handling for PUT/GET but putQueue not yet coded

--- IdeaProjects/ds-asm-2/src/main/java/org/example/AggregationServer.java
+++ IdeaProjects/ds-asm-2/src/main/java/org/example/AggregationServer.java
@@ -6,10 +6,15 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonArray;
 
+// data structures
 import java.io.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
+
+// socket programming
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class AggregationServer {
     // ----- config + identity -----
@@ -18,7 +23,7 @@ public class AggregationServer {
     private final String persistent_dir;        // directory where WAL/snapshots are stored
     private volatile boolean running = true;
     private final AtomicLong arrival_seq = new AtomicLong(0);
-    private final ExecutorService connection_pool = Executor.newCachedThreadPool();
+    private final ExecutorService connection_pool = Executor.newCachedThreadPool(); // for new connection to server socket
 
     // ----- in-memory/persistent storage -----
     private final Map<String, WeatherRecord> memory_store = new ConcurrentHashMap<>();
@@ -50,6 +55,11 @@ public class AggregationServer {
         this.port = port;
         this.lp_clock = new LamportClock(node_id);
         this.persis_manager = new PersistenceManager(persistent_dir);
+        load_snapshot_WAL();
+
+        // ---- TODO ----
+        start_worker();
+        start_expiry_checker();
     }
 
     // Load snapshot + WAL on startup in case of recovery after crash
@@ -93,6 +103,117 @@ public class AggregationServer {
             }
         } catch (Exception e) {
             System.err.println("Failed to load snapshot/WAL" + e.getMessage());
+        }
+    }
+
+    // Starting the aggregation server and start accepting request
+    private void start() throws IOException {
+        try (ServerSocket server = new ServerSocket(port)) {
+            System.out.println("Aggregation Server listening on port " + port);
+            // Accpeting requests while on
+            while (running) {
+                Socket s = server.accept();
+                connection_pool.submit(() -> handle_connection(s));
+            }
+        } catch (java.lang.Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // ---- HTTP Request Handling -----
+    private void handle_connection(Socket s) {
+        try(BufferedInputStream buffer_input = new BufferedInputStream(s.getInputStream());
+         OutputStream out_stream = s.getOutputStream()){
+            BufferedReader reader = new BufferedReader(new InputStreamReader(buffer_input, StandardCharsets.UTF_8));
+
+            // read each line from the request
+            String request_line = reader.readLine();
+            if (request_line == null) return;
+
+            // 400 - Requests are not either GET or PUT
+            String[] parts = request_line.split(" ");
+            string method = parts[0];
+            if (!method.equals("GET") && !method.equals("PUT")) {
+                write_response(out_stream, 400, "Bad Request");
+            }
+
+            // read header
+            long content_length = 0;
+            String content_type = "";
+            long remote_lamport = -1;
+            String line;
+            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                int index = line.indexOf(":");
+
+                if (index > 0) {
+                    String key = line.substring(0, index).trim();
+                    String value = line.substring(index + 1).trim();
+
+                    if (key.equalsIgnoreCase("Content-Length")){
+                        content_length = Long.parseLong(value);
+                    } else if (key.equalsIgnoreCase("Content-Type")){
+                        content_type = String.parseString(value);
+                    } else if (key.equalsIgnoreCase("X-Lamport-Clock")){
+                        remote_lamport = Long.parseLong(value);
+                    }
+                }
+            }
+
+            // Update Aggregation Server Lamport Clock when receive request
+            if (remote_lamport >= 0) {
+                lp_clock.on_receive(remote_lamport);
+            }
+
+            // ----- Handling PUT Request -----
+            if ("PUT".equalsIgnoreCase(method)){
+                if (content_length == 0) {
+                    write_response(out_stream, 204, "No Content");
+                    return;
+                }
+
+                // reading the request body
+                char[] body_buffer = new char[(int) content_length];
+                int actual_read = 0;
+                // parse character buffer into body
+                while (actual_read < content_length) {
+                    int read_len = reader.read(body_buffer, actual_read, (int) (content_length - actual_read));
+                    if (read_len < 0) break;
+                    actual_read += read_len;
+                }
+                // example: body = "{\"id\":\"123\",\"temp\":25,\"humidity\":80}"
+                String body = new String(body_buffer, 0, Math.max(0, actual_read));
+
+                // Convert body into JSON object
+                JsonObject payload;
+                try {
+                    payload = JsonParser.parseString(body).getAsJsonObject();
+                } catch (Exception e){
+                    write_response(out_stream, 500, "Invalid JSON!");
+                    return;
+                }
+
+                // 500 - Missing id
+                String id = payload.has("id") ? payload.get("id").getAsString() : null;
+                if (id == null || id.isEmpty()) {
+                    write_response(out_stream, 500, "Missing id in payload!");
+                    return;
+                }
+
+                // Prepare PUT request for writer to update
+                long lamport_header = (remote_lamport >= 0) ? remote_lamport : lp_clock.tick();         // if lp_clock from content sv smaller -> use agg sv lp_clock
+                PutRequest req = new PutRequest(lamport_header, arrival_seq.IncrementAndGet(), payload.deepCopy());
+                put_queue.put(req);
+
+                int result = req.result_future().get();
+                write_resposne(out_stream,result, result == 201? "Created" : "OK");
+            }
+        }
+        catch (Exception e) {
+
+        } finally {
+            try {
+                s.close();
+            } catch (IOException ignored) {}
         }
     }
 }
